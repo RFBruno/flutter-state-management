717657b41c9eda3f688f6077f50c474167434b73
Bruno Rafael <bruno_rafael_08@hotmail.com>
2022-12-23 15:26:08 -0300

    Added get orders firebase

--- lib/models/order_list.dart
+++ lib/models/order_list.dart
@@ -2,11 +2,14 @@ import 'dart:convert';
 
 import 'package:flutter/cupertino.dart';
 import 'package:shop/models/cart.dart';
+import 'package:shop/models/cart_item.dart';
 import 'package:shop/models/order.dart';
 import 'package:http/http.dart' as http;
 import 'package:shop/utils/constants.dart';
 
 class OrderList with ChangeNotifier {
+  final _baseUrl = Constants.ordersBaseUrl;
+
   List<Order> _items = [];
 
   List<Order> get items => [..._items];
@@ -21,15 +24,15 @@ class OrderList with ChangeNotifier {
         {
           'total': cart.totalAmount,
           'data': date.toIso8601String(),
-          'products': cart.items.values.map(
-            (cartItem) => {
-                'id': cartItem.id,
-                'productId': cartItem.productId,
-                'name': cartItem.name,
-                'quantity': cartItem.quantity,
-                'price': cartItem.price,
-              }
-            ).toList()
+          'products': cart.items.values
+              .map((cartItem) => {
+                    'id': cartItem.id,
+                    'productId': cartItem.productId,
+                    'name': cartItem.name,
+                    'quantity': cartItem.quantity,
+                    'price': cartItem.price,
+                  })
+              .toList()
         },
       ),
     );
@@ -43,6 +46,34 @@ class OrderList with ChangeNotifier {
         date: DateTime.now(),
       ),
     );
+    notifyListeners();
+  }
+
+  Future<void> loadOrders() async {
+    _items.clear();
+
+    final response = await http.get(
+      Uri.parse('$_baseUrl.json'),
+    );
+
+    if (jsonDecode(response.body) == null) return;
+    Map<String, dynamic> data = jsonDecode(response.body);
+    data.forEach((orderId, orderData) {
+      _items.add(Order(
+        id: orderId,
+        total: double.parse(orderData['total']),
+        products: (orderData['products'] as List<dynamic>).map((el) {
+          return CartItem(
+            id: orderId,
+            productId: orderId,
+            name: el['name'],
+            quantity: el['quantity'],
+            price: el['price'],
+          );
+        }).toList(),
+        date: DateTime.parse( orderData['data']),
+      ));
+    });
     notifyListeners();
   }
 }
--- lib/pages/oders_page.dart
+++ lib/pages/oders_page.dart
@@ -2,15 +2,30 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shop/components/app_drawer.dart';
 import 'package:shop/components/order.dart';
+import 'package:shop/models/order.dart';
 import 'package:shop/models/order_list.dart';
 
-class OrdersPage extends StatelessWidget {
+class OrdersPage extends StatefulWidget {
   const OrdersPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    final OrderList orders = Provider.of(context);
+  State<OrdersPage> createState() => _OrdersPageState();
+}
 
+class _OrdersPageState extends State<OrdersPage> {
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    Provider.of<OrderList>(context, listen: false).loadOrders();
+    
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final OrderList orders = Provider.of(context);  
     return Scaffold(
       appBar: AppBar(
         title: const Text('Meus pedidos'),
